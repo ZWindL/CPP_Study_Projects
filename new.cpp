f770d0c87e482f99309142f5cacf0b1de39f781a
zwindl <lizefeng1996@gmail.com>
2015-08-29 17:43:39 +0800

    +

--- new.cpp
+++ new.cpp
@@ -1,9 +1,23 @@
+/*
+ * 除了开头
+ * 注释掉的地方
+ * 都是錯误的
+ */
+
 #include <iostream>
 #include "new.h"
 
 int main()
 {
-	extern const int a;
-	std::cout << a << std::endl;
+	int i = 1;
+	const int j = 2;	//int 常量类型
+
+	int *const p = &i;	//(顶层 const) 只读类型指針，可以指向任意类型的 int 类型
+	//int *const p1 = &j;	//(顶层 const) 指針本身只读，但不限定指向的內容只读
+	const int *const p1 = &j;	//(底层 const) 限定指向的內容不可更改
+
+	int &r = i;
+	//int &r1 = j;
+	
 	return 0;
 }
--- new.h
+++ new.h
@@ -1,3 +1,7 @@
+/*
+ * 这个头文件的格式是固定的
+ */
+
 #include <iostream>
 
 const int a = 1;
